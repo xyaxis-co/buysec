fee2b0aaa2f8a7a1d9c40fa6bec8edaf5332d2dd
harshalkaigaonkar <harshalkaigaonkar31@gmail.com>
2021-11-23 23:05:39 +0530

    build: inst build

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,11 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+const  App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="animate-bounce text-red-700">Under Construction !!</h1>
       </header>
     </div>
   );
