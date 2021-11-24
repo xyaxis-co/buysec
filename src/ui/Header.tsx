58dddc6fe999f132d283257052d83abfcd103c0f
harshalkaigaonkar <harshalkaigaonkar31@gmail.com>
2021-11-24 14:13:52 +0530

    build: Built Header with some minor interactions left

--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -1,14 +1,18 @@
 import React, { useState } from 'react'
 import Logo from '../assets/svgs/Logo.svg';
 
+interface Handler {
+    (): void
+}
+
 const Header: React.FC<any> = () => {
     const [search, setSearch] = useState<string>('invisible');
-    const onSearchHandler = (): void => {
+    const onSearchHandler: Handler = (): void => {
         setSearch('visible');
     }
     return (
         <>
-            <div className=" inset-0 text-2xl bg-white shadow-sm flex flex-row items-center h-24 font-black">
+            <div className="w-full inset-0 text-2xl bg-white shadow-sm flex flex-row items-center h-24 font-black">
                 <div className="w-1/6 flex flex-row justify-center">
                     <a href="/" >
                         <img src={Logo} alt="Logo"/>
@@ -17,26 +21,26 @@ const Header: React.FC<any> = () => {
                 <div className="w-1/6 text-primary">
                     <p className="cursor-pointer">Categories</p>
                 </div>
-                <div className="w-2/6 text-primary">
+                <div className="mx-3.5 w-2/6 text-primary">
                     <input type="text" placeholder="Search" autoFocus id="signin" className={`${search} w-full border-4 rounded-3xl border-tertiary px-3 py-1.5`}/>
                 </div>
-                <div className="w-2/6 flex flex-row items-center justify-evenly">
-                    <div className="text-primary" onClick={onSearchHandler}>
-                        <button ><i className="fas fa-search"></i></button>
+                <div className="mx-10 w-2/6 flex flex-row items-center justify-evenly">
+                    <div className="text-primary mx-1.5" onClick={onSearchHandler}>
+                        <button><i className="fas fa-search"></i></button>
                     </div>
-                    <div className="hidden border-4 rounded-3xl border-tertiary text-primary px-4 hover:bg-tertiary hover:text-white">
+                    <div className="mx-2 hidden border-4 rounded-3xl border-tertiary text-primary px-4 hover:bg-tertiary hover:text-white">
                         <a href="/signin">Sign In</a>
                     </div>
-                    <div className="border-4 rounded-3xl border-tertiary text-primary hover:text-white px-4 hover:bg-tertiary" >
+                    <div className="mx-2 border-4 rounded-3xl border-tertiary text-primary hover:text-white px-4 hover:bg-tertiary" >
                         <a href="/sell">Sell</a>
                     </div>
-                    <div className="text-primary">
+                    <div className="mx-2 text-primary">
                         <a href="/chats"><i className="fas fa-comment"></i></a>
                     </div>
-                    <div className="text-primary">
+                    <div className="mx-2 text-primary">
                         <button><i className="fas fa-bell"></i></button>
                     </div>
-                    <div className="text-primary">
+                    <div className="mx-2 text-primary">
                         <a href="/person"><i className="fas fa-user-alt"></i></a>
                     </div>
                 </div>
