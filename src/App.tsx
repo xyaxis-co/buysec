df368007e702ba761dd0d58a6e038a23dc2f966c
harshalkaigaonkar <harshalkaigaonkar31@gmail.com>
2021-12-10 00:00:30 +0530

    style: responsive topbar and added address tag

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import Header from './ui/header/Header';
-// import Topbar from './ui/filters/Topbar';
+import Header from './ui/header/header';
+import Topbar from './ui/filters/topbar';
 import Signin from './ui/auth/signin';
-// import Loading from './ui/extras/Loading';
+import Loading from './ui/extras/loading';
+import Address from './ui/pages/address';
 
 const  App: React.FC<any> = () => {
   return (
     <div className="h-screen">
       <Header />
-      {/* <Topbar /> */}
-      <Signin />
+      <Address />
+      <Topbar />
+      {/* <Signin /> */}
       {/* <Loading /> */}
     </div>
   );
--- src/ui/filters/Topbar.tsx
+++ src/ui/filters/Topbar.tsx
@@ -5,23 +5,23 @@ const Topbar: React.FC<any>= () => {
         <>
             <div className="flex justify-center">
                 <div className="w-3/6 text-gray shadow hover:shadow-md bg-white flex flex-row items-center justify-evenly border-0 m-5 rounded-full p-1 ">
-                    <div className="w-1/4 text-2xl cursor-arrow flex flex-row items-center justify-center">
+                    <div className="w-2/4 lg:text-2xl text-lg cursor-arrow flex flex-row items-center justify-center">
                         <h1>All Products</h1>
                     </div>
                     <div className="w-1/4 flex-1" />
                     <div className="w-1/4 flex flex-row items-center justify-center cursor-pointer">
-                        <div className=" text-2xl">
+                        <div className="lg:text-2xl text-lg">
                             <h3>Sort</h3>
                         </div>
-                        <div className="m-3 text-xl">
+                        <div className="m-3 lg:text-xl text-base">
                             <i className="fas fa-sort"></i>
                         </div>
                     </div>
                     <div className="w-1/4 flex flex-row items-center justify-center cursor-pointer">
-                        <div className=" text-2xl">
+                        <div className="lg:text-2xl text-lg">
                             <h3>Filters</h3>
                         </div>
-                        <div className="m-3 text-xl">
+                        <div className="m-3 lg:text-xl text-base">
                             <i className="fas fa-sliders-h"></i>
                         </div>
                     </div>
